975eb2027ede12772fdd67d8f04792840e19ec80
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-13 18:45:25 +0000

    Create users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,2 @@
+-- create table users, If the table doesn't exist, create it
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255));
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+Tasks using Advanced MYSQL
