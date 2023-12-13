38f11e949ebb87d2ba316b770e623809206779bc
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-13 19:46:49 +0000

    Create users with more attributes

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,4 @@
 -- creates a table users if not exists with id, email, name, country attribute
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country CHAR(2) CHECK (country IN('US', 'CO', 'IN')) NOT NULL DEFAULT 'US');
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'IN') NOT NULL DEFAULT 'US');
