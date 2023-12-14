0b075acc06200c1fa4ab84fab66bbdbfd0fcd6bc
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-14 06:56:00 +0000

    Bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,26 @@
+-- Create the AddBonus stored procedure
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER //
+DROP TRIGGER IF EXISTS AddBonus;
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+	INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+       INSERT INTO projects(name)
+	    VALUES(project_name);
+    END IF;
+    SELECT id
+	INTO project_id
+	FROM projects
+	WHERE name = project_name;
+INSERT INTO corrections(user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+END //
+
+DELIMITER ;
