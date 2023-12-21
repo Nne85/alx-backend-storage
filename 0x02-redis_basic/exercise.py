593dcdccdfaca5a223ae97e61d7ab631e848b5c2
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-21 00:36:27 +0000

    writing redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,3 +27,24 @@ class Cache:
 
         # Return the generated key
         return key
+
+    def get(self, key: str, fn: Callable[[bytes], Any] = None) -> Any:
+        """ Retrieve data from Redis based on the provided key"""
+        data = self._redis.get(key)
+        if data is None:
+            return None  # Mimic Redis.get behavior if key doesn't exist
+
+        if fn is not None:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves data as a string, decoding bytes if necessary.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Retrieves data as an integer. """
+        return self.get(key, fn=int)
