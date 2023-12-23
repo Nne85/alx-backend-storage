400cd66805c831cae39df3f4934cd395f0eeaf3b
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-23 20:21:11 +0000

    Url Requests

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,31 @@
 #!/usr/bin/env python3
-"""This module obtain the HTML content of a particular URL"""
-
-from functools import wrapper
-import redis
+"""
+A module with tools for request caching and tracking.
+"""
 import requests
+import redis
+
 
 redis_client = redis.Redis()
 
 
-def count_access(method: Callable) -> Callable:
-    """Increments the access count for a URL in Redis using incr."""
-    @wraps(method)
-    def wrapper(url: str, *args, **kwargs):
-        """Increment access"""
-        count_key = f"count:{url}"
-        redis_client.incr(count_key)
-        return method(url, *args, **kwargs)
-    return wrapper
-
-
-def cache_result(expiration_time: int = 10) -> Callable:
-    """Returns the cached result if found"""
-    def decorator(method: Callable) -> Callable:
-        """Checks for a cached result in Redis using get"""
-        @wraps(method)
-        def wrapper(url: str, *args, **kwargs):
-            """caches the new result using setex, and returns it."""
-            cache_key = f"result:{url}"
-            result = redis_client.get(cache_key)
-            if result:
-                return result.decode("utf-8")
-
-            result = method(url, *args, **kwargs)
-            redis_client.setex(cache_key, expiration_time, result)
-            return result
-        return wrapper
-    return decorator
-
-
-@count_access
-@cache_result(expiration_time=10)
 def get_page(url: str) -> str:
-    """Fetches the HTML content of a URL using requests.get."""
-    response = requests.get(url)response.raise_for_status()
-    return response.text
+    """
+    Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    """
+    cached_content = redis_client.get(url)
+    if cached_content:
+        print("Content retrieved from cache.")
+        return cached_content.decode('utf-8')
+
+    print("Fetching content from the web...")
+    response = requests.get(url)
+    content = response.content.decode('utf-8')
+
+    redis_client.setex(url, 10, content)
+
+    access_count_key = f"count:{url}"
+    redis_client.incr(access_count_key)
+
+    return content
